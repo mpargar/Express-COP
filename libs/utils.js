7aaaca8d51b9254ead9fe160f73285f2edca43aa
Miguel Parra García <32663960+mpargar@users.noreply.github.com>
2020-04-29 16:20:08 -0600

    User seeder

---
+++ libs/utils.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt");
+module.exports = {
+  /**
+   * Esta funcion encripta contraseñas
+   * @param {String} password
+   * @returns {String} encripted password
+   * */
+  hashPassword: (password) => bcrypt.hashSync(password, 10),
+  /**
+   * Esta funcion verifica que una contraseña esta encriptada o no
+   * @param {String} plaintTextPassword Contraseña sin encriptar
+   * @param {String} encryptedPassword Contraseña encriptada
+   * @returns {Boolean}
+   * */
+  verifyPassword: (plaintTextPassword, encryptedPassword) =>
+    bcrypt.compareSync(plaintTextPassword, encryptedPassword),
+};
